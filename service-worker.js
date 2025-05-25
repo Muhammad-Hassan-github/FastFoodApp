8e19875a165ed3595e71f0514e48522a98178164
unknown <Muhammad-Hassan-github>
2025-05-25 19:06:35 +0500

    first commit3

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,10 @@
+// Version: v2 - force update due to phone number change
 self.addEventListener("install", e => {
-  console.log("Service Worker Installed");
+  console.log("Service Worker Installed - v2");
+});
+
+self.addEventListener("activate", e => {
+  console.log("Service Worker Activated");
 });
 
 self.addEventListener("fetch", function (event) {
